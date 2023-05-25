f9095b4f2e5f112ee66cfa6c6b63e7b0c9692c58
MateusMeloFernandes <125471307+MateusMeloFernandes@users.noreply.github.com>
2023-05-25 15:55:54 -0300

    Add files via upload

---
+++ PSET.sql
@@ -0,0 +1,167 @@
+
+DROP DATABASE IF EXISTS uvv;
+
+DROP USER IF EXISTS mateus;
+
+DROP SCHEMA IF EXISTS lojas CASCADE;
+
+CREATE USER mateus WITH CREATEDB INHERIT LOGIN PASSWORD 'computacao@raiz';
+
+CREATE DATABASE uvv
+owner mateus
+template template0
+encoding 'UTF8'
+lc_collate 'pt_BR.UTF-8'
+lc_ctype 'pt_BR.UTF-8' 
+allow_connections TRUE;
+
+
+\c 'dbname=uvv user=mateus password=computacao@raiz'
+
+CREATE SCHEMA lojas AUTHORIZATION mateus;
+ALTER USER mateus SET SEARCH_PATH TO lojas, "$user", public;
+SET SEARCH_PATH TO lojas, "$user", public;
+GRANT ALL PRIVILEGES ON SCHEMA lojas TO mateus;
+
+CREATE TABLE produtos (
+                produto_id NUMERIC(38) NOT NULL,
+                nome VARCHAR(255) NOT NULL,
+                preco_unitario NUMERIC(10,2),
+                detalhes BYTEA,
+                imagem BYTEA,
+                imagem_mime_type VARCHAR(512),
+                imagem_arquivo VARCHAR(512),
+                imagem_charset VARCHAR(512),
+                imagem_ultima_atualizacao DATE,
+                CONSTRAINT produto_id_pk PRIMARY KEY (produto_id)
+);
+COMMENT ON TABLE produtos IS 'tabela contendo as informações dos produtos';
+
+
+CREATE TABLE lojas (
+                loja_id NUMERIC(38) NOT NULL,
+                nome VARCHAR(255) NOT NULL,
+                endereco_web VARCHAR(100),
+                endereco_fisico VARCHAR(512),
+                latitude NUMERIC,
+                longitude NUMERIC,
+                logo BYTEA,
+                logo_mime_type VARCHAR(512),
+                logo_arquivo VARCHAR(512),
+                logo_charset VARCHAR(512),
+                logo_ultima_atualizacao DATE,
+                CONSTRAINT loja_id_pk PRIMARY KEY (loja_id)
+);
+COMMENT ON TABLE lojas IS 'tabela contendo as informações das lojas';
+
+
+CREATE TABLE estoques (
+                estoque_id NUMERIC(38) NOT NULL,
+                loja_id NUMERIC(38) NOT NULL,
+                produto_id NUMERIC(38) NOT NULL,
+                quantidade NUMERIC(38) NOT NULL,
+                CONSTRAINT estoque_id_pk PRIMARY KEY (estoque_id)
+);
+COMMENT ON TABLE estoques IS 'tabela contendo informações dos estoques das lojas';
+
+
+CREATE TABLE clientes (
+                cliente_id NUMERIC(38) NOT NULL,
+                email VARCHAR(255) NOT NULL,
+                nome VARCHAR(255) NOT NULL,
+                telefone1 VARCHAR(20),
+                telefone2 VARCHAR(20),
+                telefone3 VARCHAR(20),
+                CONSTRAINT cliente_id_pk PRIMARY KEY (cliente_id)
+);
+COMMENT ON TABLE clientes IS 'tabela contendo as informações dos clientes';
+
+
+CREATE TABLE envios (
+                envio_id NUMERIC(38) NOT NULL,
+                loja_id NUMERIC(38) NOT NULL,
+                cliente_id NUMERIC(38) NOT NULL,
+                endereco_entrega VARCHAR(512) NOT NULL,
+                status VARCHAR(15) NOT NULL,
+                CONSTRAINT envio_id_pk PRIMARY KEY (envio_id)
+);
+COMMENT ON TABLE envios IS 'tabela contendo informações de todos os envios entre as lojas e clientes';
+
+
+CREATE TABLE pedidos (
+                pedido_id NUMERIC(38) NOT NULL,
+                data_hora TIMESTAMP NOT NULL,
+                cliente_id NUMERIC(38) NOT NULL,
+                status VARCHAR(15) NOT NULL,
+                CONSTRAINT pedido_id_pk PRIMARY KEY (pedido_id)
+);
+COMMENT ON TABLE pedidos IS 'tabela contendo os pedidos feitos nas lojas';
+
+
+CREATE TABLE pedidos_itens (
+                pedido_id NUMERIC(38) NOT NULL,
+                produto_id NUMERIC(38) NOT NULL,
+                numero_da_linha NUMERIC(38) NOT NULL,
+                preco_unitario NUMERIC(10,2) NOT NULL,
+                quantidade NUMERIC(38) NOT NULL,
+                envio_id NUMERIC(38),
+                CONSTRAINT pedido_id_pk_e_produto_id PRIMARY KEY (pedido_id, produto_id)
+);
+COMMENT ON TABLE pedidos_itens IS 'tabela contendo informações de todos os itens pedidos';
+
+
+ALTER TABLE pedidos_itens ADD CONSTRAINT produtos_pedidos_itens_fk
+FOREIGN KEY (produto_id)
+REFERENCES produtos (produto_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE estoques ADD CONSTRAINT produtos_estoques_fk
+FOREIGN KEY (produto_id)
+REFERENCES produtos (produto_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE estoques ADD CONSTRAINT lojas_estoques_fk
+FOREIGN KEY (loja_id)
+REFERENCES lojas (loja_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE envios ADD CONSTRAINT lojas_envios_fk
+FOREIGN KEY (loja_id)
+REFERENCES lojas (loja_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE pedidos ADD CONSTRAINT clientes_pedidos_fk
+FOREIGN KEY (cliente_id)
+REFERENCES clientes (cliente_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE envios ADD CONSTRAINT clientes_envios_fk
+FOREIGN KEY (cliente_id)
+REFERENCES clientes (cliente_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE pedidos_itens ADD CONSTRAINT envios_pedidos_itens_fk
+FOREIGN KEY (envio_id)
+REFERENCES envios (envio_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE pedidos_itens ADD CONSTRAINT pedidos_pedidos_itens_fk
+FOREIGN KEY (pedido_id)
+REFERENCES pedidos (pedido_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
